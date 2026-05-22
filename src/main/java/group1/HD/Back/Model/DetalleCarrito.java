d866953f3a8f80243ca2db3247f52d88625e3985
C3s4r310 <cu046481@gmail.com>
2026-05-22 18:56:58 -0500

    feat: Detalle carrito

---
+++ src/main/java/group1/HD/Back/Model/DetalleCarrito.java
@@ -0,0 +1,41 @@
+package group1.HD.Back.model;
+
+public class DetalleCarrito {
+
+    private Long idProducto;
+    private String nombreProducto;
+    private int cantidad;
+
+    public DetalleCarrito() {
+    }
+
+    public DetalleCarrito(Long idProducto, String nombreProducto, int cantidad) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
