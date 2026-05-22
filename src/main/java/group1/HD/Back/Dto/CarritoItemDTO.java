17772a25b24515d9bd3bbd2763c180b8aa3617e8
C3s4r310 <cu046481@gmail.com>
2026-05-22 18:57:48 -0500

    Feat: DTO

---
+++ src/main/java/group1/HD/Back/Dto/CarritoItemDTO.java
@@ -0,0 +1,41 @@
+package group1.HD.Back.dto;
+
+public class CarritoItemDTO {
+
+    private Long idProducto;
+    private String nombreProducto;
+    private int cantidad;
+
+    public CarritoItemDTO() {
+    }
+
+    public CarritoItemDTO(Long idProducto, String nombreProducto, int cantidad) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
