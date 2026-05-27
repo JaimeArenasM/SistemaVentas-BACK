7eca0196c28c588db71a3fcca32bb2742ff6f441
C3s4r310 <cu046481@gmail.com>
2026-05-27 04:03:52 +0000

    feat: Producto

---
+++ src/main/java/group1/HD/Back/Model/Producto.java
@@ -0,0 +1,41 @@
+package group1.HD.Back.model;
+
+public class Producto {
+
+    private Long idProducto;
+    private String nombre;
+    private Double precio;
+
+    public Producto() {
+    }
+
+    public Producto(Long idProducto, String nombre, Double precio) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
