7b5c72329f4d51d3fb59c69de2d89a0ee5bb07b4
C3s4r310 <cu046481@gmail.com>
2026-05-27 04:01:36 +0000

    feat: agregar manejo global de excepciones

---
+++ src/main/java/group1/HD/Back/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package group1.HD.Back.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    public Map<String, String> manejarValidaciones(
+            MethodArgumentNotValidException ex
+    ) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errores.put(
+                        error.getField(),
+                        error.getDefaultMessage()
+                )
+        );
+
+        return errores;
+    }
+}
