267748397f2452457d58061d7bd6f9cb3cb02c11
C3s4r310 <cu046481@gmail.com>
2026-05-27 03:57:53 +0000

    feat: agregar validaciones al carrito item dto

--- src/main/java/group1/HD/Back/Dto/CarritoItemDTO.java
+++ src/main/java/group1/HD/Back/Dto/CarritoItemDTO.java
@@ -1,8 +1,15 @@
 package group1.HD.Back.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class CarritoItemDTO {
 
+    @NotNull(message = "El id del producto es obligatorio")
     private Long idProducto;
+
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 1, message = "La cantidad debe ser mayor a 0")
     private Integer cantidad;
 
     public CarritoItemDTO() {
