60f2153bc66991f745fb0504f2e976274b0d1180
ArbiManuel <u23225486@utp.edu.pe>
2026-05-27 19:17:37 -0500

    Modelos categoria y producto listos

---
+++ src/main/java/group1/HD/Back/Model/Categoria.java
@@ -0,0 +1,44 @@
+package group1.HD.Back.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String nombre;
+
+    private String descripcion;
+
+    // GETTERS Y SETTERS
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
