9b4317cee811b2720ac1a6c62be719f52966b014
JaimeArenasM <jaimealejandro987@gmail.com>
2026-05-28 11:19:09 -0500

    Se crearon endpoints Rest adicionales em VentaController donde en las lineas donde se crearon esta un comentario sobre su funcion individual de cada uno de estos endpoints

--- src/main/java/group1/HD/Back/Controller/VentaController.java
+++ src/main/java/group1/HD/Back/Controller/VentaController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import group1.HD.Back.Model.EstadoVenta;
 import group1.HD.Back.Model.Venta;
 import group1.HD.Back.Service.VentaService;
 
@@ -20,8 +24,8 @@ public class VentaController {
     @Autowired
     private VentaService ventaService;
 
-    @PostMapping
-    public Venta crearVenta() {
+    @PostMapping("/checkout")
+    public Venta checkout() {
         return ventaService.checkout();
     }
 
@@ -29,4 +33,24 @@ public class VentaController {
     public List<Venta> obtenerVentas() {
         return ventaService.obtenerVentas();
     }
-}
+    @GetMapping("/{id}")
+    public Venta obtenerVentaPorId(@PathVariable Long id) {
+
+        return ventaService.obtenerVentaPorId(id);
+    }
+
+    @PutMapping("/{id}/estado")
+    public Venta cambiarEstado(
+            @PathVariable Long id,
+            @RequestParam EstadoVenta estado
+    ) {
+
+        return ventaService.cambiarEstado(id, estado);
+    }
+
+    @GetMapping("/mis-compras")
+    public List<Venta> misCompras() {
+
+        return ventaService.obtenerVentas();
+    }
+    }
