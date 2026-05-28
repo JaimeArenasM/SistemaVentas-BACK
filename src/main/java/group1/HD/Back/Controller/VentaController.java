7f73bebbacede9655389467b71288ac09686373a
JaimeArenasM <jaimealejandro987@gmail.com>
2026-05-27 23:03:32 -0500

    Creacion del controlador VentaController

---
+++ src/main/java/group1/HD/Back/Controller/VentaController.java
@@ -0,0 +1,32 @@
+package group1.HD.Back.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import group1.HD.Back.Model.Venta;
+import group1.HD.Back.Service.VentaService;
+
+@RestController
+@RequestMapping("/api/ventas")
+@CrossOrigin("*")
+public class VentaController {
+
+    @Autowired
+    private VentaService ventaService;
+
+    @PostMapping
+    public Venta crearVenta() {
+        return ventaService.crearVenta();
+    }
+
+    @GetMapping
+    public List<Venta> obtenerVentas() {
+        return ventaService.obtenerVentas();
+    }
+}
