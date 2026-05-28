0ddc91c2a193667e4b44d24551ac0c13a79fc634
JaimeArenasM <jaimealejandro987@gmail.com>
2026-05-27 22:57:41 -0500

    Creacion de VentaRepository

---
+++ src/main/java/group1/HD/Back/Repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package group1.HD.Back.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import group1.HD.Back.Model.Venta;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
