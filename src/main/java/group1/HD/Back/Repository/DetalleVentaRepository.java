114349cfc2268a361887b80287c3d75831383c83
JaimeArenasM <jaimealejandro987@gmail.com>
2026-05-27 23:42:09 -0500

    Continuacion del commit anterior(descuido de guardar)

--- src/main/java/group1/HD/Back/Repository/DetalleVentaRepository.java
+++ src/main/java/group1/HD/Back/Repository/DetalleVentaRepository.java
@@ -3,5 +3,5 @@ package group1.HD.Back.Repository;
 import group1.HD.Back.Model.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DetalleRepository extends JpaRepository<DetalleVenta, Long> {
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
 }
