38668cdd25984a76a8c6d5583d94a247db5125cc
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-06-01 21:52:48 -0500

    fix: arreglar en la variable de expiracion de token, Jwt

--- src/main/java/group1/HD/Back/Security/Jwt.java
+++ src/main/java/group1/HD/Back/Security/Jwt.java
@@ -7,19 +7,21 @@ import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+@Component
 public class Jwt {
     
     @Value("${jwt.secret}")
     private String secretKey;
 
     @Value("${jwt.expiration}")
-    private String jwtExpiration;
+    private long jwtExpiration;
     
     private SecretKey getSignInKey(){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
