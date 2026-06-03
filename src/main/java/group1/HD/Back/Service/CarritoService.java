31eff1a29e9bdfd73139c0561f3b5e06f7de0276
C3s4r310 <cu046481@gmail.com>
2026-06-03 17:08:18 +0000

    refactor: limpia CarritoService y usa Streams para lógica de items

--- src/main/java/group1/HD/Back/Service/CarritoService.java
+++ src/main/java/group1/HD/Back/Service/CarritoService.java
@@ -1,6 +1,7 @@
 package group1.HD.Back.service;
 
-import group1.HD.Back.dto.CarritoItemDTO;
+import group1.HD.Back.Dto.Request.CarritoRequest;
+import group1.HD.Back.Dto.Response.CarritoResponse;
 import group1.HD.Back.model.Carrito;
 import group1.HD.Back.model.DetalleCarrito;
 
@@ -9,56 +10,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class CarritoService {
 
+    // TODO: Reemplazar por persistencia en BD filtrando por el usuario autenticado
     private final Carrito carrito = new Carrito();
 
-    public Carrito obtenerCarrito() {
-        return carrito;
+    public CarritoResponse obtenerCarrito() {
+        return mapToResponse(carrito);
     }
 
-    public void agregarProducto(CarritoItemDTO dto) {
-
-        for (DetalleCarrito item : carrito.getItems()) {
-
-            if (item.getIdProducto().equals(dto.getIdProducto())) {
-
-                item.setCantidad(
-                        item.getCantidad() + dto.getCantidad()
+    public CarritoResponse agregarProducto(CarritoRequest request) {
+        carrito.getItems().stream()
+                .filter(item -> item.getIdProducto().equals(request.getProductoId()))
+                .findFirst()
+                .ifPresentOrElse(
+                        item -> item.setCantidad(item.getCantidad() + request.getCantidad()),
+                        () -> carrito.getItems().add(new DetalleCarrito(request.getProductoId(), request.getCantidad()))
                 );
 
-                return;
-            }
-        }
-
-        DetalleCarrito nuevoItem = new DetalleCarrito(
-                dto.getIdProducto(),
-                dto.getCantidad()
-        );
-
-        carrito.getItems().add(nuevoItem);
+        return mapToResponse(carrito);
     }
 
-    public void actualizarCantidad(Long idProducto, Integer cantidad) {
+    public CarritoResponse actualizarCantidad(Long idProducto, Integer cantidad) {
+        carrito.getItems().stream()
+                .filter(item -> item.getIdProducto().equals(idProducto))
+                .findFirst()
+                .ifPresent(item -> item.setCantidad(cantidad));
 
-        for (DetalleCarrito item : carrito.getItems()) {
-
-            if (item.getIdProducto().equals(idProducto)) {
-
-                item.setCantidad(cantidad);
-
-                return;
-            }
-        }
+        return mapToResponse(carrito);
     }
 
-    public void eliminarProducto(Long idProducto) {
-
-        carrito.getItems().removeIf(
-                item -> item.getIdProducto().equals(idProducto)
-        );
+    public CarritoResponse eliminarProducto(Long idProducto) {
+        carrito.getItems().removeIf(item -> item.getIdProducto().equals(idProducto));
+        return mapToResponse(carrito);
     }
 
-    public void limpiarCarrito() {
-
+    public CarritoResponse limpiarCarrito() {
         carrito.getItems().clear();
+        return mapToResponse(carrito);
+    }
+
+    private CarritoResponse mapToResponse(Carrito carritoModel) {
+        CarritoResponse response = new CarritoResponse();
+        
+        // TODO: Mapear datos reales desde la entidad persistida
+        response.setId(1L);
+        response.setNombreUsuario("Usuario en sesión");
+        response.setTotalItems(carritoModel.getItems().size());
+        response.setTotalPagar(0.0); // TODO: Calcular total dinámicamente
+        
+        return response;
     }
 }
