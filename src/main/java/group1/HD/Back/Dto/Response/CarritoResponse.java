135535f5d14f32046987fb35c92b2a895e7ada2f
C3s4r310 <cu046481@gmail.com>
2026-06-04 13:05:27 -0500

    improve: CarritoResponse

--- src/main/java/group1/HD/Back/Dto/Response/CarritoResponse.java
+++ src/main/java/group1/HD/Back/Dto/Response/CarritoResponse.java
@@ -1,65 +1,19 @@
 package group1.HD.Back.Dto.Response;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class CarritoResponse {
+    private Integer idCarrito;
+    private BigDecimal totalCarrito;
+    private List<DetalleCarritoResponse> items;
 
-    private Long id;
-    private String nombreUsuario; // O clienteId, dependiendo de tu modelo
-    private Integer totalItems;   // Cuántos productos hay en el carrito
-    private Double totalPagar;    // El monto total a pagar
-    private LocalDateTime fechaActualizacion;
-
-    // Constructores
-    public CarritoResponse() {
+    public CarritoResponse(Integer idCarrito, BigDecimal totalCarrito, List<DetalleCarritoResponse> items) {
+        this.idCarrito = idCarrito;
+        this.totalCarrito = totalCarrito;
+        this.items = items;
     }
-
-    public CarritoResponse(Long id, String nombreUsuario, Integer totalItems, Double totalPagar, LocalDateTime fechaActualizacion) {
-        this.id = id;
-        this.nombreUsuario = nombreUsuario;
-        this.totalItems = totalItems;
-        this.totalPagar = totalPagar;
-        this.fechaActualizacion = fechaActualizacion;
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public Integer getTotalItems() {
-        return totalItems;
-    }
-
-    public void setTotalItems(Integer totalItems) {
-        this.totalItems = totalItems;
-    }
-
-    public Double getTotalPagar() {
-        return totalPagar;
-    }
-
-    public void setTotalPagar(Double totalPagar) {
-        this.totalPagar = totalPagar;
-    }
-
-    public LocalDateTime getFechaActualizacion() {
-        return fechaActualizacion;
-    }
-
-    public void setFechaActualizacion(LocalDateTime fechaActualizacion) {
-        this.fechaActualizacion = fechaActualizacion;
-    }
+    public Integer getIdCarrito() { return idCarrito; }
+    public BigDecimal getTotalCarrito() { return totalCarrito; }
+    public List<DetalleCarritoResponse> getItems() { return items; }
 }
