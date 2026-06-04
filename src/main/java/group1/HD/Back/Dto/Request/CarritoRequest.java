4018ae5b8324e52919a8814a0ac86d02915e37c0
C3s4r310 <cu046481@gmail.com>
2026-06-04 13:12:48 -0500

    improve: CarritoRequest

--- src/main/java/group1/HD/Back/Dto/Request/CarritoRequest.java
+++ src/main/java/group1/HD/Back/Dto/Request/CarritoRequest.java
@@ -1,3 +1,5 @@
+dto request
+
 package group1.HD.Back.Dto.Request;
 
 import jakarta.validation.constraints.Min;
@@ -6,35 +8,15 @@ import jakarta.validation.constraints.NotNull;
 public class CarritoRequest {
 
     @NotNull(message = "El id del producto es obligatorio")
-    private Long productoId;
+    private Integer idProducto;
 
     @NotNull(message = "La cantidad es obligatoria")
     @Min(value = 1, message = "La cantidad debe ser mayor a 0")
     private Integer cantidad;
 
-    // Constructores
-    public CarritoRequest() {
-    }
-
-    public CarritoRequest(Long productoId, Integer cantidad) {
-        this.productoId = productoId;
-        this.cantidad = cantidad;
-    }
-
-    // Getters y Setters
-    public Long getProductoId() {
-        return productoId;
-    }
-
-    public void setProductoId(Long productoId) {
-        this.productoId = productoId;
-    }
-
-    public Integer getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(Integer cantidad) {
-        this.cantidad = cantidad;
-    }
+    public CarritoRequest() {}
+    public Integer getIdProducto() { return idProducto; }
+    public void setIdProducto(Integer idProducto) { this.idProducto = idProducto; }
+    public Integer getCantidad() { return cantidad; }
+    public void setCantidad(Integer cantidad) { this.cantidad = cantidad; }
 }
